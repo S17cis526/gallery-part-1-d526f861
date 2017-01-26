b5afe68a961760b70d199a21ce39da758c64e87e
David Freeman <davidfreeman@ksu-wireless-3-26.campus.ksu.edu>
2017-01-26 14:20:31 -0600

    created basic gallery

--- server.js
+++ server.js
@@ -1,6 +1,58 @@
+"use strict";
+
 /**
  * server.js
  * This file defines the server for a
  * simple photo gallery web app.
  */
- 
+var http = require('http');
+var fs = require('fs');
+var port = 3000;
+
+function serveImage(filename, req, res) {
+  fs.readFile('images/' + filename, (err, body) => {
+    if(err) {
+      console.error(err);
+      res.statusCode = 500;
+      res.statusMessage = "Server Error";
+      res.end();
+      return;
+    }
+    res.setHeader("Content-Type", "image/jpeg");
+    res.end(body);
+  });
+}
+
+var server = http.createServer((req, res) => {
+
+  switch(req.url) {
+    case "/ace":
+      serveImage('ace.jpg', req, res);
+      break;
+
+    case "/bubble":
+      serveImage('bubble.jpg', req, res);
+      break;
+
+    case "/chess":
+      serveImage('chess.jpg', req, res);
+      break;
+
+    case "/fern":
+      serveImage('fern.jpg', req, res);
+      break;
+
+    case "/mobile":
+      serveImage('mobile.jpg', req, res);
+      break;
+
+    default:
+      res.statusCode = 404;
+      res.statusMessage = "Not Found";
+      res.end();
+  }
+});
+
+server.listen(port, () => {
+  console.log("Listening on Port " + port);
+});
